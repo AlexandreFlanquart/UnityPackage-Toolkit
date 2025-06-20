06a4b4c37d61a0c33a4a01d7310f017c83ed841a
Victor-Simon1 <115700243+Victor-Simon1@users.noreply.github.com>
2025-06-20 16:52:43 +0200

    :sparkles: add logger features

---
+++ Runtime/Logger/Logger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Logger
+{
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogMessageEditor(string message)
+    {
+        Debug.Log(message);
+    }
+    public static void LogMessage(string message)
+    {
+        Debug.Log(message);
+    }
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogMessageWarningEditor(string message)
+    {
+        Debug.LogWarning(message);
+    }
+
+    public static void LogMessageError(string message)
+    {
+        Debug.LogError(message);
+    }
+}
