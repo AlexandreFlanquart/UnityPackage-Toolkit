0c286ac2a979eb6c5aea4737afa9cb4f745ccf7c
Victor-Simon1 <115700243+Victor-Simon1@users.noreply.github.com>
2025-06-25 14:40:28 +0200

    :rocket: add sceneLoader

---
+++ Runtime/SceneLoader/SceneLoader.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    [SerializeField] private string[] sceneToLoad;
+    public event Action OnLoadingEvent;
+
+    IEnumerator Start()
+    {
+        yield return LoadSceneList(sceneToLoad);
+    }
+
+    private IEnumerator LoadSceneList(string[] list)
+    {
+        foreach (var name in list)
+        {
+            AsyncOperation asyncOp = SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+            while (asyncOp.isDone == false)
+            {
+                yield return null;
+            }
+        }
+        OnLoadingEvent?.Invoke();
+        OnLoadingEvent = null;
+    }
+    private void UnloadSceneList(string[] list)
+    {
+        foreach (var name in list)
+        {
+            SceneManager.UnloadSceneAsync(name);
+        }
+    }
+    public void LoadSceneList(string[] sceneToLoadList, string[] sceneToUnLoadList)
+    {
+        UnloadSceneList(sceneToUnLoadList);
+        StartCoroutine(LoadSceneList(sceneToLoadList));
+    }
+
+}
